c7a70d3bfae66bac2573413af0dae7339f63edb3
marius a. eriksen <marius@monkey.org>
2009-10-20 10:27:04 -0700

    typechecking of base32 input, and of (lat, lon)s

--- Data/Geohash.hs
+++ Data/Geohash.hs
@@ -11,8 +11,7 @@
 -- Note that the implementation pays little regard to performance at
 -- this point.
 -- 
--- TODO: - check ranges of input, return Maybe.
---       - return precision/error values in decode?
+-- TODO: return precision/error values in decode?
 
 module Data.Geohash
   ( encode
@@ -27,7 +26,8 @@ import Data.List
 
 -- base-32 encoding using the table in the Wikipedia article.
 b32chr = "0123456789bcdefghjkmnpqrstuvwxyz"
-b32 = array (0, length b32chr) $ zip [0..length b32chr] b32chr
+b32 = array (0, l) $ zip [0..l] b32chr
+  where l = length b32chr
 b32_ = array (48, 122) $ [(i, indexOfChr (chr i)) | i <- [48..122]]
   where indexOfChr c = maybe (-1) id (elemIndex c b32chr)
 
@@ -50,10 +50,21 @@ b32encode bits =
     index :: Int
     index = bools2bits $ reverse this
 
-b32decode [] = []
-b32decode (c:cs) =
-  (reverse . take 5 $ bits2bools bits) ++ b32decode cs
-  where bits = b32_!(ord c)
+
+b32decode :: String -> Maybe [Bool]
+b32decode s =
+  if all wellDefined $ map ord s
+    then Just $ decode s
+    else Nothing
+  where
+    wellDefined ix = 
+      if inRange (bounds b32_) ix
+        then b32_!ix /= -1
+        else False
+    decode [] = []
+    decode (c:cs) =
+      (reverse . take 5 $ bits2bools bits) ++ decode cs
+      where bits = b32_!(ord c)
 
 bitstring (beg, end) x
   | x > mid   =  True:bitstring (mid, end) x
@@ -68,17 +79,23 @@ unbitstring (beg, end) (x:xs)
   where 
     mid = beg + ((end - beg)/2)
 
--- | Encode a geohash from the given @(lat, lon)@ pair. We return an
--- infinite list, so make sure to take only your needed precision.
+-- | Encode a geohash from the given @(lat, lon)@ pair with the given
+-- precision.
 -- 
--- > take 10 $ encode (37.775, -122.419)
--- "9q8yyk9pqd"
-encode :: (Fractional a, Ord a) 
-       => (a, a)                -- ^ @(lat, lon)@ pair
-       -> String                -- ^ geohash
-encode (lat, lon) =
-  b32encode $ interleave lonBits latBits
+-- > encode 10 (37.775, -122.419)
+-- Just "9q8yyk9pqd"
+-- > encode 10 (37.775, -222.419)
+-- Nothing
+encode :: (Fractional a, Ord a)
+       => Int                   -- ^ precision
+       -> (a, a)                -- ^ @(lat, lon)@ pair
+       -> Maybe String          -- ^ geohash
+encode precision (lat, lon) =
+  if inRange (-180, 180) lon && inRange (-90, 90) lat
+    then Just $ take precision $ b32encode $ interleave lonBits latBits
+    else Nothing
   where
+    inRange (beg, end) x = x >= beg && x <= end
     -- only deals with infinites, so we don't care about the []-case.
     interleave (a:as) bs  = a:(interleave' as bs)
     interleave' as (b:bs) = b:(interleave  as bs)
@@ -89,30 +106,34 @@ encode (lat, lon) =
 -- | Decode a geohash into a @(lat, lon)@ pair.
 -- 
 -- > decode "9q8yyk9pqd"
--- (37.775000631809235,-122.4189966917038)
+-- Just (37.775000631809235,-122.4189966917038)
 decode :: (Fractional a) 
        => String                -- ^ geohash
-       -> (a, a)                -- ^ @(lat, lon)@
-decode geohash = 
-  (latmin + (latmax - latmin) / 2, lonmin + (lonmax - lonmin) / 2)
-  where
-    ((latmin, latmax), (lonmin, lonmax)) = decode_ geohash
+       -> Maybe (a, a)          -- ^ @(lat, lon)@
+decode geohash =
+  case decode_ geohash of
+    Just ((latmin, latmax), (lonmin, lonmax)) ->
+      Just (latmin + (latmax - latmin) / 2, lonmin + (lonmax - lonmin) / 2)
+    Nothing -> Nothing
 
 -- | Decode a geohash into ranges of latitude & longitude. This
 -- reflects the precision of the decode and can be used to construct a
 -- bounding box.
 -- 
 -- > decode_ "9q8yyk9pqd"
--- ((37.77499794960022,37.77500331401825),(-122.41900205612183,-122.41899132728577))
+-- Just ((37.77499794960022,37.77500331401825),(-122.41900205612183,-122.41899132728577))
 decode_ :: (Fractional a) 
-        => String               -- ^ geohash
-        -> ((a, a), (a, a))     -- ^ @((latmin, latmax), (lonmin, lonmax))@
+        => String                  -- ^ geohash
+        -> Maybe ((a, a), (a, a))  -- ^ @((latmin, latmax), (lonmin, lonmax))@
 decode_ geohash =
-  (unbitstring (-90, 90) latBits, unbitstring (-180, 180) lonBits)
+  case b32decode geohash of
+    Just bits -> 
+      let (lonBits, latBits) = deinterleave bits in
+      Just $ (unbitstring (-90, 90)   latBits, 
+              unbitstring (-180, 180) lonBits)
+    Nothing -> 
+      Nothing
   where 
-    bits = b32decode geohash
-    (lonBits, latBits) = deinterleave bits
-
     -- We can't simply unzip with a simpler deinterleave because xs
     -- and ys aren't guaranteed to be of equal length.
     deinterleave xs@(_:ys) = (deinterleave' xs, deinterleave' ys)
